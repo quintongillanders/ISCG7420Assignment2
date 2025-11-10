0f1a4c7cb00386dc15eaaeffcc282aa1751b21d6
quintongillanders <quingillanders@gmail.com>
2025-11-10 21:34:02 +1300

    Updated settings.py and views.py for adding email notifications

--- DjangoProject (2)/DjangoProject/DjangoProject/settings.py
+++ DjangoProject (2)/DjangoProject/DjangoProject/settings.py
@@ -120,3 +120,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'quingillanders@gmail.com'
+EMAIL_HOST_PASSWORD = 'xoqhkbddxlqbpipc'
